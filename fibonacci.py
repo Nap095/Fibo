77d9568d6f0f540466d837b96c02b3dbe0fd63fe
Nap095 <did6095@gmail.com>
2021-08-06 19:57:21 +0200

    version avec lrucache

--- fibonacci.py
+++ fibonacci.py
@@ -1,18 +1,13 @@
-fibonacci_cache = {}
+from functools import lru_cache
 
+@lru_cache(maxsize=1000)
 def fibonacci(n):
-    if n in fibonacci_cache:
-        return fibonacci_cache[n]
-
-    if n==1:
-        value = 1
-    elif n==2:
-        value = 1
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 1
     elif n > 2:
-        value = fibonacci(n-1) + fibonacci(n-2)
-    
-    fibonacci_cache[n] = value
-    return value
+        return fibonacci(n-1) + fibonacci(n-2)
 
-for n in  range(1, 101):
+for n in range(1, 501):
     print(n, ":", fibonacci(n))

e8170b42e5f330520113a3b43c4391c6c52312c4
Nap095 <did6095@gmail.com>
2021-08-06 19:48:53 +0200

    version avec cache

--- fibonacci.py
+++ fibonacci.py
@@ -1,10 +1,18 @@
-def fibonacci(n):
-    if n == 1:
-        return 1
-    elif n == 2:
-        return 1
-    elif n > 2:
-        return fibonacci(n-1) + fibonacci(n-2)
+fibonacci_cache = {}
 
-for n in range(1, 101):
+def fibonacci(n):
+    if n in fibonacci_cache:
+        return fibonacci_cache[n]
+
+    if n==1:
+        value = 1
+    elif n==2:
+        value = 1
+    elif n > 2:
+        value = fibonacci(n-1) + fibonacci(n-2)
+    
+    fibonacci_cache[n] = value
+    return value
+
+for n in  range(1, 101):
     print(n, ":", fibonacci(n))
